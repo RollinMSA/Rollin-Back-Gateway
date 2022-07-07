90774b2ad510957aebfeef9bbafbbfc25ef898c6
jeen0202 <71309053+jeen0202@users.noreply.github.com>
2022-07-07 21:33:37 +0900

    resillience 시도중

--- src/main/java/com/example/rollin_gateway/RollinGatewayApplication.java
+++ src/main/java/com/example/rollin_gateway/RollinGatewayApplication.java
@@ -1,11 +1,18 @@
 package com.example.rollin_gateway;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
 import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+
+import java.time.Duration;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -17,5 +24,16 @@ public class RollinGatewayApplication {
     @Bean
     public HttpTraceRepository httpTraceRepository(){
         return new InMemoryHttpTraceRepository();
+    }
+    @Bean
+    public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                .circuitBreakerConfig(CircuitBreakerConfig.custom()
+                        .slidingWindowSize(100)
+                        .failureRateThreshold(50.0f)
+                        .waitDurationInOpenState(Duration.ofMillis(30000))
+                        .build())
+                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofMillis(200)).build())
+                .build());
     }
 }
